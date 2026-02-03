c3b5a7157fd8884c76d4b37c691f7ab261fb735f
ebo-uf <kms3177583@naver.com>
2026-02-04 02:52:07 +0900

    [FEAT] OpenFeign CircuitBreaker 구현 Product 서비스에서 User 서비스를 호출할 때 User 서비스가 죽어 있는 경우 FallBack되도록 함

--- common/src/main/java/mall/common/feign/UserFeignClient.java
+++ common/src/main/java/mall/common/feign/UserFeignClient.java
@@ -1,11 +1,12 @@
 package mall.common.feign;
 
 import mall.common.dto.UserResponseDto;
+import mall.common.feign.fallback.UserFeignFallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient("user-service")
+@FeignClient(name = "user-service", fallbackFactory = UserFeignFallbackFactory.class)
 public interface UserFeignClient {
     @GetMapping("/users/me")
     UserResponseDto findById(@RequestHeader("Authorization") String authHeader);
---
+++ common/src/main/java/mall/common/feign/fallback/UserFeignFallbackFactory.java
@@ -0,0 +1,22 @@
+package mall.common.feign.fallback;
+
+import lombok.extern.slf4j.Slf4j;
+import mall.common.dto.UserResponseDto;
+import mall.common.feign.UserFeignClient;
+import org.springframework.cloud.openfeign.FallbackFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class UserFeignFallbackFactory implements FallbackFactory<UserFeignClient> {
+    @Override
+    public UserFeignClient create(Throwable cause) {
+        return new UserFeignClient() {
+            @Override
+            public UserResponseDto findById(String authHeader) {
+                log.error("User Service 호출 중 에러 발생: {}", cause.getMessage());
+                throw new RuntimeException("현재 유저 서비스 이용이 불가능합니다. 잠시 후 다시 시도해주세요.");
+            }
+        };
+    }
+}
