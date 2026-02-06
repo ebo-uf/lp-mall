757b0f9069a6dfd985dcecbce45adeaa4d89c0cc
ebo-uf <kms3177583@naver.com>
2026-02-07 05:42:09 +0900

    [CHORE] cors 문제 해결 gateway에서 생기던 cors 문제를 해결

--- common/src/main/java/mall/common/security/SecurityConfig.java
+++ common/src/main/java/mall/common/security/SecurityConfig.java
@@ -21,9 +21,7 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
-			.csrf(AbstractHttpConfigurer::disable)
-			// 서버가 세션을 저장하지 않는다 ( 정책 )
+		http.csrf(AbstractHttpConfigurer::disable)
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers("/auth/login", "/auth/register", "/auth/logout", "/users/**", "/products/**", "/orders/**")
@@ -32,22 +30,6 @@ public class SecurityConfig {
 				.authenticated())
 			.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 		return http.build();
-	}
-
-	@Bean
-	public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
-		org.springframework.web.cors.CorsConfiguration configuration = new org.springframework.web.cors.CorsConfiguration();
-
-		// 프론트엔드 주소 허용 (리액트 기본 포트 3000)
-		configuration.addAllowedOrigin("http://localhost:3000");
-
-		configuration.addAllowedMethod("*"); // GET, POST, PUT, DELETE 모두 허용
-		configuration.addAllowedHeader("*"); // 모든 Header 허용
-		configuration.setAllowCredentials(true); // 자격 증명(쿠키, 인증 헤더 등) 허용
-
-		org.springframework.web.cors.UrlBasedCorsConfigurationSource source = new org.springframework.web.cors.UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
 	}
 
 	@Bean
